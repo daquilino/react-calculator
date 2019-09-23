9917e7cbb7e81b70c702bf2a889abade53c23bfa
D <douglasjaquilino@gmail.com>
2019-09-22 21:19:55 -0400

    simplified display component

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import './display.css'
 
-class Display extends Component {
 
-    constructor(props){
-        super(props);
-        this.state ={value: props.value}
-    }
-    
-    onChange = (event)=> this.setState({value: event.target.value});
-
-    render() {
-        console.log("display renders")
-        return (<input type="text" value={this.props.value} onChange={this.onChange}></input>)
-    }
+function display (props){
+    return (<div>{props.results = 0}</div>)
 }
+
+
+
+
+
+// Future more advanced display
+// class Display extends Component {
+
+//     constructor(props){
+//         super(props);
+//         this.state ={value: props.value}
+//     }
+    
+//     onChange = (event)=> this.setState({value: event.target.value});
+
+//     render() {
+//         console.log("display renders")
+//         return (<input type="text" value={this.props.value} onChange={this.onChange}></input>)
+//     }
+// }
 
 export default Display;
 
