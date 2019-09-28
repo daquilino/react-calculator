f1558257f71ca833cbe393750f6262893e9eb3ce
D <douglasjaquilino@gmail.com>
2019-09-27 23:11:10 -0400

    fixed syntax errors

--- src/App.js
+++ src/App.js
@@ -22,30 +22,30 @@ class App extends Component {
 
   outside = "outside";
 
-  clear = ()=>{
+  clear = () => {
     this.decimalFlag = false;
-    this.setState({num1:0, num2:0, result:0, operator:null});
+    this.setState({ num1: 0, num2: 0, result: 0, operator: null });
   }
 
-  click = (data, role)=>{
-  
-      console.log("data:", data, "role:", role);
+  click = (data, role) => {
 
-      //disables decimal button when pressed as to only use once.
-      if(data === ".") this.decimalFlag = true;
+    console.log("data:", data, "role:", role);
 
-      //this means we are still entering first number
-      if(!this.state.operator && role === "number"){
+    //disables decimal button when pressed as to only use once.
+    if (data === ".") this.decimalFlag = true;
 
-      }
+    //this means we are still entering first number
+    if (!this.state.operator && role === "number") {
 
-      if(roll === "operator") this.setState{operator: data}
+    }
+
+    if (role === "operator") this.setState({ operator: data });
 
 
-      this.setState({result: "hello"})
+    this.setState({ result: "hello" })
   }
 
-  calculate = ()=>{
+  calculate = () => {
     console.log("...calculating")
   }
 
@@ -53,38 +53,37 @@ class App extends Component {
     return (
       <main className="container">
         <Row>
-         <h6>op:{this.state.operator}</h6> 
-        <div className="calculator col-md-3" >
-          <Row>
-            <Display display={this.state.display} />
-          </Row>
-          <Row>
-            <Button name={7} buttonrole={"number"} onClick={this.click}/>
-            <Button name={8} buttonrole={"number"} onClick={this.click}/>
-            <Button name={9} buttonrole={"number"} onClick={this.click}/>
-            <Button name={"+"} buttonrole={"operator"} onClick={this.click}/>
-          </Row>
-          <Row>
-            <Button name={4} buttonrole={"number"} onClick={this.click}/>
-            <Button name={5} buttonrole={"number"} onClick={this.click}/>
-            <Button name={6} buttonrole={"number"} onClick={this.click}/>
-            <Button name={"-"} buttonrole={"operator"} onClick={this.click}/>
-          </Row>
-          <Row>
-            <Button name={1} buttonrole={"number"} onClick={this.click}/>
-            <Button name={2} buttonrole={"number"} onClick={this.click}/>
-            <Button name={3} buttonrole={"number"} onClick={this.click}/>
-            <Button name={"x"} buttonrole={"operator"} onClick={this.click}/>
-          </Row>
-          <Row>
-            <Button name={"."} buttonrole={"number"} onClick={this.click} disabled={this.decimalFlag}/>
-            <Button name={0} buttonrole={"number"} onClick={this.click}/>
-            <Button name={"C"} onClick={this.clear} />
-            <Button name={"/"} buttonrole={"operator"} onClick={this.click}/>
-          </Row>
-          <Row><Button name={"="} col={12} onClick={this.calculate}/></Row>
+          <div className="calculator col-md-3" >
+            <Row>
+              <Display display={this.state.display} />
+            </Row>
+            <Row>
+              <Button name={7} buttonrole={"number"} onClick={this.click} />
+              <Button name={8} buttonrole={"number"} onClick={this.click} />
+              <Button name={9} buttonrole={"number"} onClick={this.click} />
+              <Button name={"+"} buttonrole={"operator"} onClick={this.click} />
+            </Row>
+            <Row>
+              <Button name={4} buttonrole={"number"} onClick={this.click} />
+              <Button name={5} buttonrole={"number"} onClick={this.click} />
+              <Button name={6} buttonrole={"number"} onClick={this.click} />
+              <Button name={"-"} buttonrole={"operator"} onClick={this.click} />
+            </Row>
+            <Row>
+              <Button name={1} buttonrole={"number"} onClick={this.click} />
+              <Button name={2} buttonrole={"number"} onClick={this.click} />
+              <Button name={3} buttonrole={"number"} onClick={this.click} />
+              <Button name={"x"} buttonrole={"operator"} onClick={this.click} />
+            </Row>
+            <Row>
+              <Button name={"."} buttonrole={"number"} onClick={this.click} disabled={this.decimalFlag} />
+              <Button name={0} buttonrole={"number"} onClick={this.click} />
+              <Button name={"C"} onClick={this.clear} />
+              <Button name={"/"} buttonrole={"operator"} onClick={this.click} />
+            </Row>
+            <Row><Button name={"="} col={12} onClick={this.calculate} /></Row>
 
-        </div>
+          </div>
         </Row>
       </main>
     );
